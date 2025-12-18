5779ec02875bdf6175d56c0691957e42a8a133f5
Aleksi Salo <aleksi.salo@csc.fi>
2025-12-18 12:45:33 +0200

    Count JSONL statistics from pretty printed JSON objects
    JSONL objects have been reformatted for readability. However, the JSONL
    statistics are probably more informative as it tells the count of objects in
    files. The stats.sh script now counts JSONLs from pretty printed JSON
    objects.

--- ops/stats.sh
+++ ops/stats.sh
@@ -1,2 +1,5 @@
-wc database/*.jsonl database/files/*/* database/solutions/*/*/error database/solutions/*/*/analysis corpus/* 
-#wc docs/database/solutions/*/*/index.rst docs/corpus/*.rst
+#wc database/*.jsonl database/files/*/* database/solutions/*/*/error database/solutions/*/*/analysis corpus/*
+
+for x in database/*.objects database/files/*/* database/solutions/*/*/error database/solutions/*/*/analysis corpus/*.objects ; do
+	printf "%s %s\n" "$(jq -c . "$x" | wc)" "$x"
+done
